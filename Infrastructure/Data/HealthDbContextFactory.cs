0eaa69a01f721825492fb594792023419a49cbe6
samadev-45 <samadev45@gmail.com>
2025-10-29 14:51:46 +0530

    registration completed

--- Infrastructure/Data/HealthDbContextFactory.cs
+++ Infrastructure/Data/HealthDbContextFactory.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+//using Microsoft.Extensions.Configuration;
 
 
-namespace Health.Infrastructure.Data
-{
-    public class HealthDbContextFactory : IDesignTimeDbContextFactory<HealthDbContext>
-    {
-        public HealthDbContext CreateDbContext(string[] args)
-        {
-            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../Health.WebApi");
+//namespace Health.Infrastructure.Data
+//{
+//    public class HealthDbContextFactory : IDesignTimeDbContextFactory<HealthDbContext>
+//    {
+//        public HealthDbContext CreateDbContext(string[] args)
+//        {
+//            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../Health.WebApi");
 
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(basePath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile("appsettings.Development.json", optional: true)
-                .AddEnvironmentVariables()
-                .Build();
+//            var configuration = new ConfigurationBuilder()
+//                .SetBasePath(basePath)
+//                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+//                .AddJsonFile("appsettings.Development.json", optional: true)
+//                .AddEnvironmentVariables()
+//                .Build();
 
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
+//            var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-            var options = new DbContextOptionsBuilder<HealthDbContext>()
-                .UseSqlServer(connectionString)
-                .Options;
+//            var options = new DbContextOptionsBuilder<HealthDbContext>()
+//                .UseSqlServer(connectionString)
+//                .Options;
 
-            return new HealthDbContext(options);
-        }
-    }
-}
+//            return new HealthDbContext(options);
+//        }
+//    }
+//}
